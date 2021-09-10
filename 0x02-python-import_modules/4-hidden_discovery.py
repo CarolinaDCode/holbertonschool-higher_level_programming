41b58bc1bcd235402a6bec49645e2bfe4a9d134b
diancarhuamanta <dianacarhuamanta03@gmail.com>
2021-09-09 20:25:47 -0700

    Who are you?

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4 as lista
+    modules = dir(lista)
+    for i in range(0, len(modules)):
+        if (modules[i][0] != '_'):
+            print(modules[i])
