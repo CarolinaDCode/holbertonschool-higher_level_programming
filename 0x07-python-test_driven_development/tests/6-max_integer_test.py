ef2144b802003d42769811b9835440d97e1d044e
diancarhuamanta <dianacarhuamanta03@gmail.com>
2021-10-05 22:35:30 -0700

    fix

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])
-"""
+"""Unittest for max_integer([..])"""
 import unittest
 max_integer = __import__('6-max_integer').max_integer
+
 
 class TestMaxInteger(unittest.TestCase):
     """ tests for max_integer
@@ -27,6 +27,7 @@ class TestMaxInteger(unittest.TestCase):
         """ tests if list has only one item
         """
         self.assertEqual(max_integer([1]), 1)
+
 
 if __name__ == '__main__':
     unittest.main()
