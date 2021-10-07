e1dbce0c220ab6fd3d335f78d224dcd9f7706221
diancarhuamanta <dianacarhuamanta03@gmail.com>
2021-10-06 18:56:42 -0700

    fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -9,7 +9,7 @@ def add_integer(a, b=98):
     If they are not, it throws a TypeError with a message.
     """
 
-    if a is None (type(a) is not int and type(a) is not float):
+    if type(a) is not int and type(a) is not float:
         raise TypeError('a must be an integer')
 
     if type(b) is not int and type(b) is not float:
