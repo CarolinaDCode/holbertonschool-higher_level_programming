553014181fe95a3f7f9951e35a8ae0c12998aaef
diancarhuamanta <dianacarhuamanta03@gmail.com>
2021-10-12 22:03:42 -0700

    fix

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""Defines a function that adds arguments"""
+"""Defines a function that creates an Object"""
 import json
 
 
 def load_from_json_file(filename):
     """
-    funtion that add arguments to a python list and
-    save them in to a file
+    function that creates an Object from a “JSON file”
     """
     with open(filename, encoding="utf-8") as f:
         return json.load(f)
