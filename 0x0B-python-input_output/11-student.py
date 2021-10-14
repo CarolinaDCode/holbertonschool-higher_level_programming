f88031a25145bba4c7de0085545738b722a6f20a
diancarhuamanta <dianacarhuamanta03@gmail.com>
2021-10-13 20:41:19 -0700

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Define a class called student"""
+
+
+class Student:
+    """constructor Student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    """
+    Retrieves a dictionary
+    representation of a Student instance
+    """
+    def to_json(self, attrs=None):
+        if type(attrs) is list and all(type(j) is str for j in attrs):
+            dictionary = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    dictionary[i] = self.__dict__[i]
+            return dictionary
+        else:
+            return self.__dict__
+
+    """
+    Replaces all attributes of student instance
+    """
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            self.__dict__[key] = value
