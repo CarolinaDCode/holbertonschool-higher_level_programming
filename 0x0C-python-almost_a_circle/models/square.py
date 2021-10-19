99b5c4040b2d44112743bbda29ff689120c12e08
diancarhuamanta <dianacarhuamanta03@gmail.com>
2021-10-18 17:34:03 -0700

    Square instance to dictionary representation

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Define the class Square"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return ("[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width))
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        if len(args) != 0:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.size = args[1]
+            if len(args) > 2:
+                self.x = args[2]
+            if len(args) > 3:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "size":
+                    self.size = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
+
+    def to_dictionary(self):
+        dic = {}
+        dic['id'] = self.id
+        dic['x'] = self.x
+        dic['size'] = self.size
+        dic['y'] = self.y
+        return dic
