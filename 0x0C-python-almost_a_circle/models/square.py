79bd63bc8f98b1a53a19aaa09c592f7c5844f3a7
diancarhuamanta <dianacarhuamanta03@gmail.com>
2021-10-18 18:54:48 -0700

    fix square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,23 +4,33 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """Class Square"""
+
     def __init__(self, size, x=0, y=0, id=None):
+        """Constructor"""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """__str__ method"""
         return ("[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width))
 
     @property
     def size(self):
+        """Getter"""
         return self.width
 
     @size.setter
     def size(self, value):
+        """Setter, validations"""
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
+        """
+        Public Method that assigns a key/value
+        argument to attributes
+        """
         if len(args) != 0:
             if len(args) > 0:
                 self.id = args[0]
@@ -42,6 +52,10 @@ class Square(Rectangle):
                     self.y = value
 
     def to_dictionary(self):
+        """
+        Public Method that returns the dictionary
+        representation of a Square
+        """
         dic = {}
         dic['id'] = self.id
         dic['x'] = self.x
