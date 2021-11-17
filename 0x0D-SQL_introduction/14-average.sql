c82bf166b345ebf201a539372fa0ae254b60a446
CarolinaDCode <dianacarhuamanta03@gmail.com>
2021-11-16 23:42:19 -0500

    computes the score average of all records in the table second_table

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- Escriba un script que calcule el promedio de puntuación de todos los registros en la tabla second_table
+-- de la base de datos hbtn_0c_0 en su servidor MySQL.
+SELECT AVG(score) AS average FROM second_table;
