5a81802eeb09fcf68fd1da76b0050da11f51af54
CarolinaDCode <dianacarhuamanta03@gmail.com>
2021-11-16 23:40:45 -0500

    removes all records with a score <= 5 in the table second_table

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- Escriba un script que elimine todos los registros con una puntuación <= 5
+-- en la tabla second_table de la base de datos hbtn_0c_0 en su servidor MySQL.
+DELETE FROM second_table WHERE score <= 5;
