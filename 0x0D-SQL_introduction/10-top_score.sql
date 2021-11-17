49daefef2be68134fa9e4904ed76c6e634e05c6f
CarolinaDCode <dianacarhuamanta03@gmail.com>
2021-11-16 23:34:55 -0500

    lists all records of the table second_table

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- Escriba un script que enumere todos los registros de la tabla second_table
+-- de la base de datos hbtn_0c_0 en su servidor MySQL
+SELECT score, name FROM second_table ORDER BY score DESC;
