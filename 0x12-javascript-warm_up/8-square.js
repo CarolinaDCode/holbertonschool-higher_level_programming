10e9829ecac112d7d15bec9e44d4957a9557eb47
CarolinaDCode <dianacarhuamanta03@gmail.com>
2021-12-13 16:59:57 -0500

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const num = parseInt(process.argv[2]);
 if (!isNaN(num)) {
-    for (let i = 0; i < num; i++) {
-        console.log('x'.repeat(num));
-    }
+  for (let i = 0; i < num; i++) {
+    console.log('X'.repeat(num));
+  }
 } else {
-    console.log('Missing size');
+  console.log('Missing size');
 }
