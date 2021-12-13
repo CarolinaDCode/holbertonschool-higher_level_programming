6c309d7f7b45f203d83371595cb7bd740f8716c8
CarolinaDCode <dianacarhuamanta03@gmail.com>
2021-12-13 17:45:22 -0500

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,7 +3,7 @@ function factorial (a) {
     if (isNaN(a) || a < 2) {
         return 1;
     } else {
-    return a * factorial(a - 1);
+        return a * factorial(a - 1);
     }
 }
 console.log(factorial(process.argv[2]));
