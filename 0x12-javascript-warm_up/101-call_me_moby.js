860105953e670ba650f30b861135d8031b7b3d60
CarolinaDCode <dianacarhuamanta03@gmail.com>
2021-12-13 23:47:06 -0500

    Call me Moby

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-//lllll
 exports.callMeMoby = function (x, thefunction) {
-    for (let i = 0; i < x; i++) {
-        thefunction();
-    }
+  for (let i = 0; i < x; i++) {
+    thefunction();
+  }
 };
