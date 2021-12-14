f0fae9cea6c36e439a21a82aaf4c0b0423f55dd7
CarolinaDCode <dianacarhuamanta03@gmail.com>
2021-12-13 22:20:10 -0500

    object

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-//replace the value
 const myObject = {
     type: 'object',
     value: 12
-};
-console.log(myObject);
-myObject.value = 89;
-console.log(myObject);
+  };
+  console.log(myObject);
+  myObject.value = 89;
+  console.log(myObject);
