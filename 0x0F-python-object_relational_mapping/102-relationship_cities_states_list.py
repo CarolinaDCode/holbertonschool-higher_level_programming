4ba202de9fefaf52694c2c66a4d61bfcddbb7cd2
CarolinaDCode <dianacarhuamanta03@gmail.com>
2021-12-29 10:48:14 -0500

    From city

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-"""
-lists all City objects from the database hbtn_0e_101_usa
-"""
-if __name__ == '__main__':
-    from relationship_state import Base, State
-    from relationship_city import City
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
-    import sys
+""" lists all City objects from the database hbtn_0e_101_usa"""
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine(
+                'mysql+mysqldb://{}:{}@localhost:3306/{}'
+                .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
+
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    data = session.query(State, City).join(City).order_by(City.id).all()
-    for state, city in data:
-        print('{}: {} -> {}'.format(city.id, city.name, state.name))
+    cities = session.query(City).order_by(City.id).all()
+    for city in cities:
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
+    session.commit()
     session.close()
