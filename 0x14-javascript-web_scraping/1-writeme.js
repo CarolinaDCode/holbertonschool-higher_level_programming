19ac2a096010b427492be309fcfa0b108ecc27ae
CarolinaDCode <dianacarhuamanta03@gmail.com>
2022-02-01 21:31:13 -0500

    Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-/*Write a script that writes a string to a file.*/
+/* Write a script that writes a string to a file. */
 const fs = require('fs');
 fs.writeFile(process.argv[2], process.argv[3], 'utf-8', function (err, data) {
   if (err) {
