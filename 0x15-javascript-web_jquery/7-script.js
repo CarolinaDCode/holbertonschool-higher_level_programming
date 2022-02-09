0837c6a21751430941aaa2e5827d630be9434f8a
CarolinaDCode <dianacarhuamanta03@gmail.com>
2022-02-09 16:21:49 -0500

    adds the name of a Swapi API character to the Div#character element

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,4 +1,3 @@
-
 // adds the name of a Swapi API character to the Div#character element
 $.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function(data, status){
     $('div#character').text(data.name);
