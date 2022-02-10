fa8d96a1e2d010d21ad42684aada5ff5eef83806
CarolinaDCode <dianacarhuamanta03@gmail.com>
2022-02-09 20:43:37 -0500

    add the <li> Item </li> element to the UL.my_list element

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,4 +1,4 @@
 // add the <li> Item </li> element to the UL.my_list element
-$('div#red_header').click(function(){
+$('DIV#add_item').click(function(){
     $('UL.my_list').append('<li>Item</li>');
 });
