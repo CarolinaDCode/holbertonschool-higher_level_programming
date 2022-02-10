83911faadb950c6dff68c5f7b57a131f011b9c09
CarolinaDCode <dianacarhuamanta03@gmail.com>
2022-02-09 21:30:44 -0500

    get the value of hello from the URL

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,8 +1,6 @@
 // get the value of hello from the URL
 // 'https://fourtonfish.com/hellosalut/?lang=fr'
 // and add it to the id hello
-$.get('https://fourtonfish.com/hellosalut/?lang=fr', function(data, status){
-    for (let movie in data.results) {
-        $('div#hello').text(data.hello);
-    }
+$.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
+  $('DIV#hello').text(data.hello);
 });
