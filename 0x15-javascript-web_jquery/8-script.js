ef8973ea4e0bced29113a399781bdbbe74e43d45
CarolinaDCode <dianacarhuamanta03@gmail.com>
2022-02-09 21:15:05 -0500

    Add a list of the titles of all the movies to the UL#list_movies element

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,6 +1,6 @@
 // Add a list of the titles of all the movies to the UL#list_movies element
 $.get('https://swapi-api.hbtn.io/api/films/?format=json', function(data, status){
     for (let movie in data.results) {
-        $('ul#list_movies').append(`<li>${movie.title}</li>`);
+        $('UL#list_movies').append(`<li>${data.results[movie].title}</li>`);
     }
 });
